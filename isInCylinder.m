bf562e0a78bc46aae43fddafc1b2ff40104742c9
Daiyao Xu <130887176+Daiyaoxu@users.noreply.github.com>
2023-09-02 16:22:11 -0400

    Delete isInCylinder.m

--- isInCylinder.m
+++
@@ -1,5 +0,0 @@
-function isIn = isInCylinder(photon_pos, diameter, cylinder_length)
-    r = sqrt(photon_pos(1,1)^2 + photon_pos(1,2)^2);
-    z = photon_pos(3);
-    isIn = (r >= diameter/2) & (r <= photon_pos*5) & (z >= 0) & (z <= cylinder_length*5);
-end
