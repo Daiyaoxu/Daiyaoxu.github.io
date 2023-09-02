d15a42a17b013dfc27952e54296232cf3a7d133d
Daiyao Xu <130887176+Daiyaoxu@users.noreply.github.com>
2023-09-02 16:22:42 -0400

    Delete randomDirection.m

--- randomDirection.m
+++
@@ -1,7 +0,0 @@
-% Functions
-function dir = randomDirection()
-    phi = 2*pi*rand();
-    cosTheta = 2*rand() - 1; % -1 to 1
-    sinTheta = sqrt(1 - cosTheta^2);
-    dir = [sinTheta*cos(phi), sinTheta*sin(phi), cosTheta];
-end
