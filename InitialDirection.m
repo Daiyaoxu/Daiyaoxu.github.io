5963ae8a7b6a86501bec1e9ae408684252e6d550
Daiyao Xu <130887176+Daiyaoxu@users.noreply.github.com>
2023-10-25 02:01:20 -0400

    Update InitialDirection.m

--- InitialDirection.m
+++ InitialDirection.m
@@ -2,17 +2,17 @@ function new_dir = InitialDirection(old_dir)
     x=old_dir(:,1);
     y=old_dir(:,2);
     
-    % 使用Henyey-Greenstein相位函数来确定散射后的方向
+    % Henyey-Greenstein相位函数
     xi2 = rand();
     cosThetaRel = xi2;
     cosThetaRel = min(max(cosThetaRel, -1), 1); 
     sinThetaRel = sqrt(1 - cosThetaRel^2);
     phiRel = 2 * pi * rand();
 
-    % 构造一个坐标系，其中old_dir是z轴
+
     [perp1, perp2] = findPerpendicularVectors([x,y,0]);
 
-    % 计算散射后的方向
+    % 散射后的方向
     scattered_dir = sinThetaRel * (cos(phiRel) * perp1 + sin(phiRel) * perp2) + cosThetaRel * [x,y,0];
     new_dir = scattered_dir / norm(scattered_dir);
 end
