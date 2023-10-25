ea33815ca0ce8e72113f1014ba77dd672157d976
Daiyao Xu <130887176+Daiyaoxu@users.noreply.github.com>
2023-10-25 02:03:02 -0400

    Update scatterDirection.m

--- scatterDirection.m
+++ scatterDirection.m
@@ -1,12 +1,13 @@
 function new_dir = scatterDirection(old_dir, g)
-    % 使用Henyey-Greenstein相位函数来确定散射后的方向
+    % Henyey-Greenstein相位函数
     xi2 = rand();
     cosThetaRel = (1 + g^2 - ((1 - g^2) / (1 + g - 2*g*xi2))^2) / (2*g);
-    cosThetaRel = min(max(cosThetaRel, -1), 1); % 限制cosThetaRel的范围在[-1, 1]
+    cosThetaRel = min(max(cosThetaRel, -1), 1); 
     sinThetaRel = sqrt(1 - cosThetaRel^2);
     phiRel = 2 * pi * rand();
 
-    % 构造一个坐标系，其中old_dir是z轴
+    
+    
     [perp1, perp2] = findPerpendicularVectors(old_dir);
 
     % 计算散射后的方向
